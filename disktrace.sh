7fd9cc5c412d6b8328a852cee3077f464db23ddb
purushothamanpoovai <purushothamanpoovai@gmail.com>
2020-12-30 11:03:49 +0530

    updated this file for showing last N data

--- disktrace.sh
+++ disktrace.sh
@@ -2,18 +2,29 @@
 #Purushothaman K
 #Keeps a history of the available free space in mounts
 
-disktracefile=/tmp/disktrace.txt 
+disktracefile=/tmp/disktrace.txt
+N=${1-5}  #Default last 5 data will be printed 
 
-df -h | tr -s ' ' | awk -F ' ' '{ print $6,$4}' | while IFS=' ' read -u 3 disk free ;do 
+(( "$N" < 1 )) && echo "Enter a number > 0" && exit 3
 
-	if grep  -q "^$disk :" "$disktracefile" ;then
+while IFS=' ' read -u 3 disk free ;do 
+
+	if [[ $free == "Avail" ]];then  #Keep the header of df as date
+		disk='Date'
+		free="`date +'%b%d|%H:%M'`"
+	fi
+
+	if grep  -q "^$disk =" "$disktracefile" ;then
 		#just add free to disk
-		sed -i "\@$disk :@s@\$@ $free@" "$disktracefile"
-	else 
+		sed -i "\@$disk =@s@\$@ $free@" "$disktracefile"
+	else
 		#Append the disk and free space in the report
- 		echo "$disk : $free" >> "$disktracefile"
+ 		echo "$disk = $free" >> "$disktracefile"
 	fi;
 
-done
+done 3< <(df -h | tr -s ' ' | awk -F ' ' '{ print $6,$4}' ) 
 
-cat "$disktracefile" | column -t -s" " 
+#print only the collected disks 
+#Show only the last N history of the data
+
+paste <( cat "$disktracefile" |  cut -d '=' -f1 ) <( cat "$disktracefile" | cut -d '=' -f 2 |  rev | cut -d' ' -f -${N} | rev ) | column -t -s" "  
